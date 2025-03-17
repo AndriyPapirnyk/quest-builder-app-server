829e516943e92aca87922dd4e17aa1e91bb3c619
AndriyPapirnyk <papirnykandriy@gmail.com>
2025-03-17 11:05:38 +0200

    adding test GET method

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require("mongoose");
+const cors = require('cors');
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 const url = process.env.MONGODB_URL;
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World Testing and connected to mongodb');
+});
+
+app.get('/get-data', (req, res) => {
+  res.send('Hello from server');
 });
 
 async function connect() {
