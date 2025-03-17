0cd5f9cccaa754a0abf7339e14e1f9ef4eb4364f
AndriyPapirnyk <papirnykandriy@gmail.com>
2025-03-17 11:00:45 +0200

    adding mongodb connection

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require("mongoose");
 dotenv.config();
 const PORT = process.env.PORT || 8000;
+const url = process.env.MONGODB_URL;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World Testing');
+  res.send('Hello World Testing and connected to mongodb');
 });
+
+async function connect() {
+  try {  
+    await mongoose.connect(url);
+    console.log(`Connected to mongodb`);
+  } catch (error) {
+    console.error(`Connection error: ${error}`);
+  }
+};
+
+connect();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
